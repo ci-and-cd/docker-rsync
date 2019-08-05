83bd65c3f68cba05e05228a151b53a0e34d185d6
haolun zhang <chshawkn@users.noreply.github.com>
2019-08-06 02:32:19 +0800

    fix docker/entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,9 +8,13 @@ RSYNC_VOLUME_NAME=${RSYNC_VOLUME_NAME:-volume}
 RSYNC_VOLUME_PATH=${RSYNC_VOLUME_PATH:-/data}
 
 if [[ "$1" = 'rsync_server' ]]; then
+    chmod u=rwx,g-rwx,o-rwx /root/.ssh
+    # If '/root/.ssh' directory is mounted from host, chmod can not change permission on it.
+    sed -Ei 's/^[#]?StrictModes[ ]+yes$/StrictModes no/' /etc/ssh/sshd_config
+
     if [[ -e "/root/.ssh/authorized_keys" ]]; then
         chown root:root /root/.ssh/authorized_keys
-        chmod 644 /root/.ssh/authorized_keys
+        chmod 600 /root/.ssh/authorized_keys
     fi
     echo "root:${RSYNC_PASSWORD}" | chpasswd
 
@@ -19,24 +23,26 @@ if [[ "$1" = 'rsync_server' ]]; then
     chmod 0400 /etc/rsyncd.secrets
     mkdir -p ${RSYNC_VOLUME_PATH}
     chown root:root ${RSYNC_VOLUME_PATH}
+    chmod g+rxw,u+rxw ${RSYNC_VOLUME_PATH}
 
     [[ -f /etc/rsyncd.conf ]] || cat <<EOF > /etc/rsyncd.conf
-    lock file = /var/lock/rsyncd.lock
-    log file = /dev/stdout
-    max connections = 10
-    pid file = /var/run/rsyncd.pid
-    port = 873
-    timeout = 300
-    [${RSYNC_VOLUME_NAME}]
-        auth users = ${RSYNC_USERNAME}
-        comment = ${RSYNC_VOLUME_PATH} directory
-        gid = root
-        hosts allow = ${RSYNC_HOSTS_ALLOW}
-        hosts deny = *
-        read only = false
-        path = ${RSYNC_VOLUME_PATH}
-        secrets file = /etc/rsyncd.secrets
-        uid = root
+auth users = ${RSYNC_USERNAME}
+gid = root
+hosts allow = ${RSYNC_HOSTS_ALLOW}
+hosts deny = *
+lock file = /var/lock/rsyncd.lock
+log file = /dev/stdout
+max connections = 10
+pid file = /var/run/rsyncd.pid
+port = 873
+read only = false
+secrets file = /etc/rsyncd.secrets
+timeout = 300
+uid = root
+#use chroot = no
+[${RSYNC_VOLUME_NAME}]
+    comment = ${RSYNC_VOLUME_PATH} directory
+    path = ${RSYNC_VOLUME_PATH}
 EOF
 
     if [[ -e "/root/host_dot_ssh/id_rsa.pub" ]]; then
